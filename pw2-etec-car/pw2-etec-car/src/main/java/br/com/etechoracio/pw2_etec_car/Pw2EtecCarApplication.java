faaccbad2a1cc0d15a1a61c86ab76b35711beb83
PedroLudovic144 <136478720+PedroLudovic144@users.noreply.github.com>
2024-08-03 12:09:02 -0300

    New commit

---
+++ pw2-etec-car/pw2-etec-car/src/main/java/br/com/etechoracio/pw2_etec_car/Pw2EtecCarApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.pw2_etec_car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw2EtecCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw2EtecCarApplication.class, args);
+	}
+
+}
